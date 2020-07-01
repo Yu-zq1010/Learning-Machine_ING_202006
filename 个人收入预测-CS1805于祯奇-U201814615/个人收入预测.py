8f763cc235dca6dda7a8f1797c3ebe31d58e44ae
Yu-zq1010 <yuzhenqi19991010@gmail.com>
2020-07-01 11:14:38 +0800

    结课项目——个人收入预测

--- 个人收入预测-CS1805于祯奇-U201814615/个人收入预测.py
+++ 个人收入预测-CS1805于祯奇-U201814615/个人收入预测.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import numpy as np
+import xlwt
 
 
 # 更新参数，训练模型
-def train(x_train, y_train, epoch):
+def train(x_train, y_train, epoch, learning_rate, reg_rate):
     num = x_train.shape[0]
     dim = x_train.shape[1]
     bias = 0  # 偏置值初始化
     weights = np.ones(dim)  # 权重初始化
-    learning_rate = 1  # 初始学习率
-    reg_rate = 0.001  # 正则项系数
     bg2_sum = 0  # 用于存放偏置值的梯度平方和
     wg2_sum = np.zeros(dim)  # 用于存放权重的梯度平方和
+    Epsilon = 1e-5
+    LossList = []
 
     for i in range(epoch):
         b_g = 0
@@ -33,35 +34,34 @@ def train(x_train, y_train, epoch):
         bias -= learning_rate / bg2_sum ** 0.5 * b_g
         weights -= learning_rate / wg2_sum ** 0.5 * w_g
 
-        # 每训练100轮，输出一次在训练集上的正确率
         # 在计算loss时，由于涉及到log()运算，因此可能出现无穷大，计算并打印出来的loss为nan
-        # 有兴趣的同学可以把下面涉及到loss运算的注释去掉，观察一波打印出的loss
-        if i % 3 == 0:
-            # loss = 0
-            acc = 0
-            result = np.zeros(num)
-            for j in range(num):
-                y_pre = weights.dot(x_train[j, :]) + bias
-                sig = 1 / (1 + np.exp(-y_pre))
-                if sig >= 0.5:
-                    result[j] = 1
-                else:
-                    result[j] = 0
 
-                if result[j] == y_train[j]:
-                    acc += 1.0
-                # loss += (-1) * (y_train[j] * np.log(sig) + (1 - y_train[j]) * np.log(1 - sig))
-            # print('after {} epochs, the loss on train data is:'.format(i), loss / num)
-            print('after {} epochs, the acc on train data is:'.format(i), acc / num)
+        loss = 0
+        acc = 0
+        result = np.zeros(num)
+        for j in range(num):
+            y_pre = weights.dot(x_train[j, :]) + bias
+            sig = 1 / (1 + np.exp(-y_pre))
+            if sig >= 0.5:
+                result[j] = 1
+            else:
+                result[j] = 0
 
-    return weights, bias
+            if result[j] == y_train[j]:
+                acc += 1.0
+            loss += (-1) * (y_train[j] * np.log(sig + Epsilon) + (1 - y_train[j]) * np.log(1 - sig + Epsilon))
+        LossList.append(loss / num)
+
+    return weights, bias, LossList
 
 
 # 验证模型效果
-def validate(x_val, y_val, weights, bias, num):
-    # loss = 0
+def validate(x_val, y_val, weights, bias):
+    loss = 0
     acc = 0
+    num = x_val.shape[0]
     result = np.zeros(num)
+
     for j in range(num):
         y_pre = weights.dot(x_val[j, :]) + bias
         sig = 1 / (1 + np.exp(-y_pre))
@@ -70,9 +70,9 @@ def validate(x_val, y_val, weights, bias, num):
         else:
             result[j] = 0
 
+
         if result[j] == y_val[j]:
             acc += 1.0
-        # loss += (-1) * (y_val[j] * np.log(sig) + (1 - y_val[j]) * np.log(1 - sig))
     return acc / num
 
 
@@ -92,15 +92,102 @@ def main():
     y = array[:, -1]
 
     # 划分训练集与验证集
-    x_train, x_val = x[0:3000, :], x[3000:, :]
-    y_train, y_val = y[0:3000], y[3000:]
+    x_train, x_val = x[:2999, :], x[2999:, :]
+    y_train, y_val = y[0:2999], y[2999:]
 
-    epoch = 50  # 训练轮数
-    # 开始训练
-    w, b = train(x_train, y_train, epoch)
-    # 在验证集上看效果
-    acc = validate(x_val, y_val, w, b, x_val.shape[0])
-    print('The acc on val data is:', acc)
+    ExcelFile = xlwt.Workbook()
+
+    # 1.不同学习率对于Loss的影响
+    print("\t验证学习率对于Loss的影响：")
+    epoch = 200
+    sheet = ExcelFile.add_sheet('学习率对Loss的影响', cell_overwrite_ok=True)
+    learning_rates = [0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10]
+    reg_rate = 0.001
+    # 准备写入工作
+    sheet.write_merge(0, 1, 0, 0, '迭代次数')
+    sheet.write_merge(0, 0, 1, len(learning_rates), 'Loss的值')
+    for i in range(epoch):
+        sheet.write(i + 2, 0, i + 1)
+
+    for i in range(len(learning_rates)):
+        print("正在计算学习率为%f时的Loss值" % learning_rates[i])
+        sheet.write(1, i + 1, 'α = ' + str(learning_rates[i]))
+        w, b, LossList = train(x_train, y_train, epoch, learning_rates[i], reg_rate)
+        for j in range(epoch):
+            sheet.write(j + 2, i + 1, LossList[j])
+
+    # 2. 不同正则化参数对于Loss的影响
+    print("\t验证正则化参数对于Loss的影响：")
+    epoch = 50
+    sheet = ExcelFile.add_sheet('正则化参数对Loss的影响', cell_overwrite_ok=True)
+    learning_rate = 1
+    reg_rates = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+    # 准备写入工作
+    sheet.write_merge(0, 1, 0, 0, '迭代次数')
+    sheet.write_merge(0, 0, 1, len(reg_rates), 'Loss的值')
+    for i in range(epoch):
+        sheet.write(i + 2, 0, i + 1)
+
+    for i in range(len(reg_rates)):
+        print("正在计算正则化参数为%f时的Loss值" % reg_rates[i])
+        sheet.write(1, i + 1, 'r = ' + str(reg_rates[i]))
+        w, b, LossList = train(x_train, y_train, epoch, learning_rate, reg_rates[i])
+        for j in range(epoch):
+            sheet.write(j + 2, i + 1, LossList[j])
+
+    # 3.不同学习率对于正确率的影响
+    print("\t验证学习率对于准确率的影响：")
+    epoch = 100
+    sheet = ExcelFile.add_sheet('学习率对准确率的影响', cell_overwrite_ok=True)
+    learning_rates = np.linspace(0.01, 20, 200)
+    reg_rate = 0.001
+    # 准备写入工作
+    sheet.write(0, 0, '学习率')
+    sheet.write(0, 1, '准确率')
+
+    for i in range(len(learning_rates)):
+        print("正在计算学习率为%f时的准确率" % learning_rates[i])
+        sheet.write(i + 1, 0, learning_rates[i])
+        w, b, LossList = train(x_train, y_train, epoch, learning_rates[i], reg_rate)
+        acc = validate(x_val, y_val, w, b)
+        sheet.write(i + 1, 1, acc)
+
+    # 4.不同正则化参数对于准确率的影响
+    print("\t验证正则化参数对于准确率的影响：")
+    epoch = 100
+    sheet = ExcelFile.add_sheet('正则化参数对准确率的影响', cell_overwrite_ok=True)
+    learning_rate = 1
+    reg_rates = np.linspace(0, 0.01, 100)
+    # 准备写入工作
+    sheet.write(0, 0, '正则化参数')
+    sheet.write(0, 1, '准确率')
+
+    for i in range(len(reg_rates)):
+        print("正在计算正则化参数为%f时的准确率" % reg_rates[i])
+        sheet.write(i + 1, 0, reg_rates[i])
+        w, b, LossList = train(x_train, y_train, epoch, learning_rate, reg_rates[i])
+        acc = validate(x_val, y_val, w, b)
+        sheet.write(i + 1, 1, acc)
+
+    # 5.不同迭代次数对于准确率的影响
+    print("\t验证迭代次数对于准确率的影响")
+    epochs = np.linspace(10, 2000, 40, dtype=int)
+    learning_rate = 1
+    reg_rate = 0.001
+    sheet = ExcelFile.add_sheet('迭代次数对于准确率的影响', cell_overwrite_ok=True)
+    # 准备写入工作
+    sheet.write(0, 0, '迭代次数')
+    sheet.write(0, 1, '准确率')
+
+    for i in range(len(epochs)):
+        print("当前迭代次数为%d" % epochs[i])
+        sheet.write(i + 1, 0, float(epochs[i]))
+        w, b, LossList = train(x_train, y_train, epochs[i], learning_rate, reg_rate)
+        acc = validate(x_val, y_val, w, b)
+        sheet.write(i + 1, 1, acc)
+
+    # 保存实验结果记录表
+    ExcelFile.save('./实验结果.xls')
 
 
 if __name__ == '__main__':
